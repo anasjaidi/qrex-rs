400662caa288a44c125cd50bb44d12c935f8d077
anas jaidi <anas.jaidi@icloud.com>
2024-06-05 00:22:50 +0100

    revert: old logic

--- src/query_builder/mod.rs
+++ src/query_builder/mod.rs
@@ -1,5 +1,6 @@
 use self::{insert_action_builder::InsertActionBuilder, where_action_builder::QueryActionBuilder};
 
+mod condition;
 mod insert_action_builder;
 mod query;
 mod where_action_builder;
@@ -10,14 +11,5 @@ pub struct QueryBuilder;
 impl QueryBuilder {
     pub fn new() -> Self {
         Self
-    }
-
-    pub fn select(&self) -> QueryActionBuilder<u32> {
-        QueryActionBuilder {
-            conditions: todo!(),
-            table: String::new(),
-            all: false,
-            fields: vec![],
-        }
     }
 }
