6ba6d310a6494429642fd1931bace0ab6d483c0b
anas jaidi <anas.jaidi@icloud.com>
2024-06-08 11:16:22 +0100

    chore: add set of todos

--- src/query_builder/mod.rs
+++ src/query_builder/mod.rs
@@ -1,5 +1,10 @@
-#![allow(unused)]
-
+/**
+* TODO: support JSON, JSONB []
+* TODO: support GEOMITRY, GEOGRAPHY []
+* TODO: support INTERVAL []
+* FIX: parse types in conditions preperly strings '' for example []
+* TODO: add procedural macro to select based on struct
+* */
 mod condition;
 mod group_by;
 mod join;
