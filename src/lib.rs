0f0b6846a9e1e8574528133a1fc72137689d4425
anas jaidi <anas.jaidi@icloud.com>
2024-06-08 12:21:34 +0100

    chore: intro to macros in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,31 @@
+#![allow(unused)]
 mod query_builder;
+
+mod tuto {
+    mod macors {
+        macro_rules! one_plus {
+            (1 + 2) => {
+                3
+            };
+            (1 + 3) => {
+                3
+            };
+            (1+ $n: literal) => {
+                1 + $n
+            };
+        }
+
+        #[test]
+        fn test_dummy0() {
+            assert_eq!(3, one_plus!(1 + 2));
+        }
+        #[test]
+        fn test_dummy1() {
+            assert_eq!(3, one_plus!(1 + 3));
+        }
+        #[test]
+        fn test_dummy2() {
+            assert_eq!(3, one_plus!(1 + 12));
+        }
+    }
+}
